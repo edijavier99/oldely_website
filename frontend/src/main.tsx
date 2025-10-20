b1d32de050aaa432aadedf57ab6e3fa34ecbc8a3
edijavier99 <edijavier10@gmail.com>
2025-10-20 15:22:47 +0100

    analytics added to the website

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './app/router';
+import { Analytics } from '@vercel/analytics/react';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
     <AppRoutes />
     </BrowserRouter>
